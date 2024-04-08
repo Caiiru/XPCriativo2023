0fc6192fc330d11a12c600110d9f9e9e07560c00
charlie.bs <115164045+charlie-bs@users.noreply.github.com>
2024-04-08 08:40:29 -0300

    Começando a implementar o projétil 2-0 e comentários no código

--- XP3BrenoBranch/Projeto/scripts/shootStuff.gd
+++ XP3BrenoBranch/Projeto/scripts/shootStuff.gd
@@ -1,25 +1,37 @@
 class_name Shooter
 extends Node2D
-
 # Cena
 @export var scene : PackedScene
 @export var Player : player
 
-var reload = 0
-var power1 = 0
+var reload = 0 # Tempo de espera para o jogador atirar novamente
+var reloadType = 0 # Os tipos são usados como uma globalização dos vários tempos de espera
+var power1 = 1
 var power2 = 0
 # Função de atirar
 func shoot(direction : Vector2) -> void:
-	if power1 == 1:
+	# tiro de fogo 1-0
+	if power1 == 1 && power2 == 0:
+		reloadType = 1
 		var new_shot := scene.instantiate() as Shot10
 		if new_shot:
 			new_shot.direction = Vector2(Player.dir, 0.0)
 			new_shot.position = Player.position
 			get_tree().root.add_child(new_shot)
+			
+	# tiro de vento 2-0
+	if power1 == 2 && power2 == 0:
+		reloadType = 2
+		var new_shot := scene.instantiate() as Shot20
+		if new_shot:
+			new_shot.direction = Vector2(Player.dir, 0.0)
+			new_shot.position = Player.position
+			get_tree().root.add_child(new_shot)
 	
-# Funcionamento Constante do Projeto
+# recarregamento dos vários projéteis
 func _process(_delta : float) -> void:
 	reload = reload - 1
 	if Input.is_action_pressed("shootInput") and reload <= 0:
 		shoot(Vector2(1.0, 0.0))
-		reload = 30
+		if reloadType == 1: reload = 60
+		if reloadType == 2: reload = 120
---
+++ XP3BrenoBranch/Projeto/scripts/shot20Logic.gd
@@ -0,0 +1,13 @@
+class_name Shot20
+extends Area2D
+
+var direction : Vector2
+
+var speed = 0
+func _physics_process(delta : float) -> void:
+	speed = speed + 10
+	position += direction * (50.0 + speed) * delta
+
+func _on_body_entered(body):
+	if body is TileMap:
+		queue_free()
